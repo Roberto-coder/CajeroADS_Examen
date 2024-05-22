b534be069b7d2c799f84bc1e2fa9daccc51039b7
Roberto-coder <61215751+Roberto-coder@users.noreply.github.com>
2024-05-22 16:16:44 -0600

    revision

--- rutas/cajero-ruta.js
+++ rutas/cajero-ruta.js
@@ -136,7 +136,7 @@ router.get('/retirar', loginControllers.ensureAuthenticated, (req, res) => {
 router.get('/transacciones', loginControllers.ensureAuthenticated, (req, res) => {
     const idCliente = req.user.id;
 
-    pool.query('SELECT * FROM transaccion WHERE idCliente = ? ORDER BY fecha DESC LIMIT 16;', [idCliente], (error, results) => {
+    pool.query("SELECT * FROM transaccion WHERE idCliente = ? ORDER BY STR_TO_DATE(fecha, '%d/%m/%Y') DESC LIMIT 16;", [idCliente], (error, results) => {
         if (error) {
             console.error('Error al obtener los datos:', error);
             return res.status(500).send('Error interno del servidor');
